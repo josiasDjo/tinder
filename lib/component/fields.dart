749c01240ef1c1cfcf3d4310860d87854f89bac8
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-04-17 19:02:54 +0200

    commit

--- lib/component/fields.dart
+++ lib/component/fields.dart
@@ -6,12 +6,17 @@ class fieldsContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height / 2 + 100,
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-      child: SizedBox(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child:
-              Image.asset("assets/images/pexels-peyman-shojaei-19137549.jpg"),
+      child: Expanded(
+        child: SizedBox(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.asset(
+              "assets/images/pexels-peyman-shojaei-19137549.jpg",
+              width: double.infinity,
+            ),
+          ),
         ),
       ),
     );
